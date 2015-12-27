2c14d3e233300ee12eae1968027d38e0feff1957
Chase Southard <chase.southard@gmail.com>
2015-12-27 13:54:21 -0500

    add tabletop to shims

--- src/script.js
+++ src/script.js
@@ -25,6 +25,9 @@ require.config({
     'flight': {
       exports: 'flight'
     },
+    'Tabletop': {
+      exports: 'Tabletop'
+    },
     'bootstrap': ['jquery'],
     leaflet: {
       exports: 'L'
